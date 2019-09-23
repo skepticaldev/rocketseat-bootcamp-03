fe80966ff3ee55bbe8ed593c1eab5f7619171e29
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-09-22 22:10:34 -0300

    Change test route 2

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,7 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.get('/', (req, res) => res.send('OK'));
+routes.get('/', (req, res) => res.send('OKDOK'));
 
 routes.use(authMiddleware);
 
